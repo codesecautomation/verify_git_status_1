68273f51fad91a1ba3bbfb60f4fbebbbe69a21d0
George Wong <wgeorge@fortinet-us.com>
2026-01-21 16:15:02 -0800

    revision

--- modules/simple_resource/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = var.bucket_name
-  # Insecure configuration for demonstration purposes
-  acl    = "public-read"
-}
-
-variable "bucket_name" {
-  type = string
-}
